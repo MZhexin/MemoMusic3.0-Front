ed21bcb8c4337547e470f97679bab1fe81034eaf
Yunhan Tian <97891129+MZhexin@users.noreply.github.com>
2023-03-02 13:57:02 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
-# 前端应用存在NPM依赖时的默认编译脚本
-npm install
-npm run build
+#!/bin/bash
+basedir=$(cd "$(dirname "$0")";pwd);
+mkdir zip && rsync -a --exclude node_modules/ --exclude package-lock.json --exclude zip/ . ./zip && cd zip && npm install --production;
+cd $basedir;
+cd zip && zip -r ../code.zip ./ -x '*.git*' -x '*.zip' -x '.DS_Store' && cd ../ && rm -rf zip;
